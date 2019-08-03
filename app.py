fedd556b3d4f20576118ede8417808596aff0a49
Animesh Risal <animeshrisal99@gmail.com>
2019-08-03 15:18:16 +0545

    add login and register function

--- app.py
+++ app.py
@@ -8,21 +8,29 @@ db = mysql.connector.connect(host="localhost", user="root", passwd="", database=
 
 cursor = db.cursor(buffered=True)
 
-
 @app.route("/")
 def index():
     return render_template("index.html")
 
+@app.route("/login")
+def login():
+    return render_template("login.html")
 
-@app.route("/admin")
+@app.route("/register")
+def register():
+    return render_template("register.html")
+
+@app.route("/admin/login")
+def admin_login():
+    return render_template("admin_login.html")
+
+@app.route("/admin/dashboard")
 def admin():
     return render_template("admin.html")
-
 
 @app.route("/profile")
 def profile():
     return render_template("profile.html")
-
 
 @app.route("/posts")
 def posts_list():
@@ -44,7 +52,7 @@ def posts_update(id):
 
 @app.route("/posts/<id>/delete")
 def posts_delete(id):
-    return render_template("post_delete.html")
+        return render_template("post_delete.html")
 
 if __name__ == "__main__":
     app.secret_key = os.urandom(12)
